fb3dd9179dfc6cde41c1b236242a8094af74b8f7
hyunsu0803 <hyuns00803@hanyang.ac.kr>
2023-10-18 08:43:59 +0000

    left the office

--- phasemap_generator.py
+++ phasemap_generator.py
@@ -3,11 +3,16 @@ import glob
 import os
 import pickle
 import random
+import scipy.signal as ss
+
 
 signal_path = './signal_samples'
 train_data = glob.glob(os.path.join(signal_path, '*.pickle'))
-size = len(train_data)
+data_size = len(train_data)
+
 fs = 16000
+Nf = 512
+win_size = 32 * 16  # 512
 
 s_len = fs*2
 
@@ -20,9 +25,10 @@ for data_path in train_data:
     signal_size = signals.shape[0]
     index = random.choice(range(signal_size - s_len))
     clipped_signals = signals[index : index + s_len, :]
+    clipped_signals = clipped_signals.T
     
     # 2. STFT
-    
-    
+    f, t, stft_signal = ss.stft(x=clipped_signals, fs=fs, nperseg=win_size, nfft=Nf)
+    print(f, t)
     
     exit()
