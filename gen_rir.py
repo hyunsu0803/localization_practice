20ed2b5556f36a1487b80a21b0a66005f1c96504
hyunsu0803 <hyuns00803@hanyang.ac.kr>
2023-11-02 08:08:45 +0000

    퇴근

--- gen_rir.py
+++ gen_rir.py
@@ -1,68 +1,76 @@
 import numpy as np
 import scipy.signal as ss
 import soundfile as sf
-import rir_generator as rir
-import matplotlib.pyplot as plt
+import rir_generator
+import os
+import json
 
 
-def rir_gen(mpos, source_pos, room_dim, rt60):
-    h = rir.generate(
-    c=340,
-    fs=16000,
-    r=mpos,             # receiver position
-    s=source_pos,       # source position
-    L=room_dim,            # room dim
-    reverberation_time=rt60,
-    nsample=4096
-    )
+class RIR:
+    def __init__(self, room):
+        # 1 rir.npy per room
+        self.room_dim = room['dim']
+        self.rt60 = room['rt60']
+        self.mcenter = room['mcenter']
+        self.room_num = room['num']
+        
+        self.marray = np.array([
+                    [-0.12, 0, 0],
+                    [-0.04, 0, 0],
+                    [0.04, 0, 0],
+                    [0.12, 0, 0]
+                    ])
+        self.dist = np.array([1, 2])
+        self.doa_range = range(0, 185, 5)
+        self.rir_length = 4096
+        self.rirs = None
+        
+    def generate_rir(self):
+        # (DOA, dist, rir, channel)
+        self.rirs = np.zeros((len(self.doa_range), 
+                              len(self.dist)+1, 
+                              self.rir_length, 
+                              len(self.marray)))
+        
+        for d in self.dist:
+            for deg in self.doa_range:
+                th = np.deg2rad(deg)
+                source_pos = np.array([d*np.cos(th), d*np.sin(th), 0]) + self.mcenter
+        
+                h = rir_generator.generate(
+                    c=340,
+                    fs=16000,
+                    r=self.marray + self.mcenter,   # receiver position
+                    s=source_pos,                   # source position
+                    L=self.room_dim,                # room dim
+                    reverberation_time=self.rt60,
+                    nsample=self.rir_length
+                    )
+                
+                doa_class = deg // 5
+                self.rirs[doa_class, d, :, :] = h
+        
+        save_dir = './files'
+        save_name = 'rir%d.npy' % self.room_num
+        np.save(os.path.join(save_dir, save_name))
+        
+        
+def main():
     
-    return h
+    with open('./files/config.json', 'r') as f:
+        config = json.load(f)
+        
+    
+    a = config['test'].split()
+    a = [float(i) for i in a]
+    print(a)
+    
+    # r1 = {'dim' : np.array([6, 6, 2.7]),
+    #       'rt60' : 0.3,
+    #       'mcenter' : ,
+    #       'num': }
+    
 
-
-signal, fs = sf.read("p287_001.wav", always_2d=True)
-print(signal.shape)
-
-# set up
-room_dim = np.array([            # room dimensions
-    [6, 6, 2.7],
-    [8, 3, 2.7]
-])
-rt60 = np.array([0.3, 0.5])
-mcenter = np.array([
-    [3, 2, 1.0],
-    [4, 0.8, 1.2]
-])
-marray = np.array([
-    [-0.12, 0, 0],
-    [-0.04, 0, 0],
-    [0.04, 0, 0],
-    [0.12, 0, 0]
-])
-"""
-rirs = np.zeros((37, 2, 2, 4096, 4))    # DOA, room, dist, rir, channel
-
-# generation
-for i in [0, 1]:
-    mpos = marray + mcenter[i]
-    # print(mpos)
-    for d in [1, 2]:
-        for t in range(0, 185, 5):
-            th = np.deg2rad(t)
-            source_pos = np.array([d*np.cos(th), d*np.sin(th), 0]) + mcenter[i]
-            h = rir_gen(mpos, source_pos, room_dim[i], rt60[i])
-            
-            doa = int(t/5)
-            rirs[doa, i, d-1, :, :] = h
-            # print(h)
-            # print(rirs[doa, i, d-1, :, :])
-            # exit()
-"""
-# np.save('training_RIRs.npy', rirs)
-rirs = np.load("training_RIRs.npy")
-
-h = rirs[0, 0, 0, :, :]
-print(h.shape)
-print(signal.shape)
-signal = ss.convolve(h[:, None, :], signal[:, :, None])
-print(signal.shape)     
-sf.write("test9.wav", signal[:, :, 0], fs)
+        
+if __name__ == '__main__':
+    main()
