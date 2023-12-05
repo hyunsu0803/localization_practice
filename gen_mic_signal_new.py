d879a2a288a5741bebf9392c0246940459137cf2
hyunsu0803 <hyuns00803@hanyang.ac.kr>
2023-12-05 08:36:26 +0000

    restart

--- gen_mic_signal_new.py
+++ gen_mic_signal_new.py
@@ -1,6 +1,6 @@
 import torch
 from torch.utils.data import Dataset, DataLoader
-import pickle
+import tqdm
 import os
 import glob
 from gen_mic_signal import MicSignal
@@ -22,6 +22,7 @@ class MicSignalDataset(Dataset):
     def __getitem__(self, index):
         s = self.speeches[index]
         self.msg.conv_n_add(s, self.rirs, self.save_path)
+        return 1
 
 
 def main():
@@ -29,18 +30,17 @@ def main():
     # training data generation
     train_speeches = "/root/mydir/hdd/librispeech/LibriSpeech/train-clean-100/**/*.flac"
     train_rooms = "rir_dir/train/*.npy"
-    save_path = "/root/mydir/hdd/training_data/mic_signal"
+    save_path = "/root/mydir/hdd/training_data/mic_signal2"
     dataset = MicSignalDataset(train_speeches, train_rooms, save_path)
-    dataloader = DataLoader(dataset, batch_size=16)
-    
+    dataloader = DataLoader(dataset, batch_size=2, num_workers=24)
 
-    # validation data generation
-    train_speeches = "/root/mydir/hdd/librispeech/LibriSpeech/dev-clean/**/*.flac"
-    train_rooms = "rir_dir/validation/*.npy"
-    save_path = "/root/mydir/hdd/validation_data/mic_signal"
-    # call_micsignal(train_speeches, train_rooms, save_path)
+    print("start")
+    num = 1
+    for i in dataloader:
+        print("batch", num)
+        num += 1
 
-    # test data generation
+    print("data generation finished")
     
     
     
